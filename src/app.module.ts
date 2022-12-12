8299b4c69667860427b81eb753574176d1326c54
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-12-12 19:56:51 +0700

    Application Configuration - Introducing the Config Module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,19 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'pass123', // user password
-      database: 'postgres', // name of our database,
+      host: process.env.DATABASE_HOST, // 'localhost', // database host
+      port: +process.env.DATABASE_PORT, // 5432, // database host
+      username: process.env.DATABASE_USER, // 'postgres', // username
+      password: process.env.DATABASE_PASSWORD, // 'pass123', // user password
+      database: process.env.DATABASE_NAME, // 'postgres', // name of our database,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: process.env.NODE_ENV === 'production' ? false : true, // your entities will be synced with the database(recommended: disable in prod)
     }),
