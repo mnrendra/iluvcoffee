0819b69d49f42c113ce5b9bb90ce8c76360ae0af
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-12-12 17:57:53 +0700

    Creating a REST API application - complete.

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,11 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
   @IsString()
   readonly name: string;
 
   @IsString()
+  @IsOptional()
+  readonly description: string | null;
+
+  @IsString()
   readonly brand: string;
+
+  @IsNumber()
+  @IsOptional()
+  readonly recommendations: number;
 
   @IsString({ each: true })
   readonly flavors: string[];
