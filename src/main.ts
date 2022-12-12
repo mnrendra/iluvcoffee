e6aac3f02f75d76950317c43a0d64976dcbf1d4d
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-12-13 01:07:55 +0700

    Generating OpenAPI Specification - Introducing the Swagger Module.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
@@ -22,8 +23,18 @@ async function bootstrap() {
   // app.useGlobalGuards(new ApiKeyGuard());
   app.useGlobalInterceptors(
     new WrapResponseInterceptor(),
-    new TimeoutInterceptor()
+    new TimeoutInterceptor(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
