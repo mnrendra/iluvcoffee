f0e776c62548ba02e188eae2a897fd0b1fea8ebf
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-12-13 01:25:07 +0700

    Generating OpenAPI Specification - Adding Example Responses.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -12,13 +12,14 @@ import {
   // UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Protocol } from 'src/common/decorators/protocol.decorator';
-// import { request } from 'http';
-import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
+import { Protocol } from '../common/decorators/protocol.decorator';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 import { Public } from '../common/decorators/public.decorator';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
 
 // @UsePipes(ValidationPipe) // Controller-scoped
 @Controller('coffees')
@@ -28,9 +29,13 @@ export class CoffeesController {
   }
 
   // @UsePipes(ValidationPipe) // Method-scoped
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
   @Public()
   @Get()
-  async findAll(@Protocol('https') protocol: string, @Query() paginationQuery: any) {
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
     console.log('CoffeesController findAll', protocol);
     // await new Promise((resolve) => setTimeout(resolve, 5000));
     return this.coffeesService.findAll(paginationQuery);
